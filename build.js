42fafe0eb3e213d567d253db5739284a260c9a07
Jeff Miller <jamiller619@gmail.com>
2020-01-09 00:42:53 -0500

    fixed warning on empty string, fixed 1.0.6 npm publish

--- build.js
+++ build.js
@@ -9,18 +9,27 @@ const src = `node_modules/tachyons/css/${filename}`
 const dest = `dist/${filename}`
 
 fs.readFile(src, (err, css) => {
+  console.log('working...\n')
   if (err) {
     return console.log(err)
   }
 
-  postcss([modules({
-    getJSON: (cssFilename, json, outputFilename) => {
-      const jsonFileName = `${dest}.json`
-      fs.writeFile(jsonFileName, JSON.stringify(json))
-    }
-  })])
+  postcss([
+    modules({
+      getJSON: (cssFilename, json, outputFilename) => {
+        const jsonFileName = `${dest}.json`
+        fs.writeFile(jsonFileName, JSON.stringify(json), err => {
+          if (err) throw err
+        })
+      }
+    })
+  ])
     .process(css, { from: src, to: dest })
     .then(result => {
-      fs.writeFile(dest, result.css, () => true)
+      fs.writeFile(dest, result.css, err => {
+        if (err) throw err
+        console.log('done!\n')
+      })
     })
+    .catch(err => console.log(err))
 })
